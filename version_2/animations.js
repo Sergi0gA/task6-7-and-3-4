e483ee2c8390fa7c73f324737688dc91805abb27
Sergi0gA <serhio.kuptsov@gmail.com>
2025-01-22 21:22:47 +0300

    Fixed version 2

--- version_2/animations.js
+++ version_2/animations.js
@@ -2,9 +2,15 @@ const navigation = document.querySelector(".desktop-nav");
 const topSection = document.querySelector(".app-header__top-section");
 const fullHeader = document.querySelector(".app-header");
 
-const threeLineCheckbox = document.querySelector(".three-line-checkbox");
+const threeLineCheckbox = document.querySelector(".three-line-button-input");
 const lines = document.querySelectorAll(".line");
 const dropoutNames = ["products", "profserv", "events", "support"];
+let dropoutCheckers = {
+  "products-dropout": false,
+  "profserv-dropout": false,
+  "events-dropout": false,
+  "support-dropout": false,
+};
 const radioNames = [
   "iaas",
   "saas",
@@ -16,15 +22,36 @@ const radioNames = [
   "outsourcing",
   "custom",
 ];
-threeLineCheckbox.addEventListener("change", () => {
-  if (threeLineCheckbox.checked) {
+let radioCheckers = {
+  "iaas-accordion": false,
+  "saas-accordion": false,
+  "dbaas-accordion": false,
+  "secaas-accordion": false,
+  "naas-accordion": false,
+  "ai-accordion": false,
+  "consulting-accordion": false,
+  "outsourcing-accordion": false,
+  "custom-accordion": false,
+};
+let threeLineClicked = false;
+threeLineCheckbox.addEventListener("click", () => {
+  threeLineClicked = !threeLineClicked;
+  if (threeLineClicked) {
     lines[0].style.transform = "translateY(6px) rotate(45deg) scaleX(1.2)";
     lines[1].style.transform = "scale(0)";
     lines[2].style.transform = "translateY(-6px) rotate(-45deg) scaleX(1.2)";
+    topSection.style.backgroundColor = "#fff";
+    topSection.style.boxShadow =
+      "0 0 6px rgba(0,0,0,.08),0 4px 16px rgba(0,0,0,.08)";
+    topSection.style.backgroundColor = "#fff";
+    navigation.classList.add("unhidden");
   } else {
     lines[0].style.transform = "";
     lines[1].style.transform = "";
     lines[2].style.transform = "";
+    topSection.style.boxShadow = "";
+    topSection.style.backgroundColor = "#f2f3f7";
+    navigation.classList.remove("unhidden");
   }
 });
 
@@ -44,48 +71,21 @@ buttons.forEach((button) => {
   });
 });
 
-threeLineCheckbox.addEventListener("change", () => {
-  if (threeLineCheckbox.checked) {
-    topSection.style.backgroundColor = "#fff";
-    topSection.style.boxShadow =
-      "0 0 6px rgba(0,0,0,.08),0 4px 16px rgba(0,0,0,.08)";
-
-    navigation.classList.add("unhidden");
-  } else {
-    topSection.style.backgroundColor = "";
-    topSection.style.boxShadow = "";
-
-    navigation.classList.remove("unhidden");
-  }
-});
-
 const accordionHeaders = document.querySelectorAll(".desktop-nav__list-header");
-
-accordionHeaders.forEach((header) => {
-  const checkbox = header.querySelector(".hidden-checkbox");
-  const content = header.nextElementSibling;
-  const accordionName = header.querySelector(".desktop-nav__header-name");
-  const accordionArrow = header.querySelector("svg");
-
-  checkbox.addEventListener("change", () => {
-    content.style.maxHeight = checkbox.checked ? "3000px" : "";
-    accordionName.style.color = checkbox.checked ? "#ff0032" : "";
-    accordionArrow.style.transform = checkbox.checked ? "rotate(-180deg)" : "";
-    accordionArrow.style.color = checkbox.checked ? "#ff0032" : "";
-  });
-});
 
 const treeAccordionHeaders = document.querySelectorAll(
   ".tree-accordion-header"
 );
 
 treeAccordionHeaders.forEach((treeHeader) => {
-  const treeCheckbox = treeHeader.querySelector(".checkbox-tree-accordion");
+  const treeButton = treeHeader.querySelector(".tree-open-button");
+  const id = treeButton.id;
   const treeContent = treeHeader.nextElementSibling;
   const verticalLine = treeHeader.querySelector(".vertical-line");
 
-  treeCheckbox.addEventListener("change", () => {
-    if (treeCheckbox.checked) {
+  treeButton.addEventListener("click", () => {
+    radioCheckers[id] = !radioCheckers[id];
+    if (radioCheckers[id]) {
       treeContent.style.maxHeight = "3000px";
       verticalLine.style.transform = "rotate(0)";
     } else {
@@ -106,125 +106,108 @@ function showMenu(event) {
   }
 }
 
-function handleCheckboxChange(event) {
-  const checkbox = event.target;
-  const header = checkbox.closest(".desktop-nav__list-header");
-  const accordionAction = header.querySelector("svg");
-  const isChecked = checkbox.checked;
+function resetButtons() {
+  Object.keys(dropoutCheckers).forEach((key) => (dropoutCheckers[key] = false));
+  document.querySelectorAll(".dropout-menu").forEach((menu) => {
+    menu.classList.remove("active-menu");
+  });
 
-  accordionAction.style.transform = isChecked ? "rotate(-180deg)" : "";
-  accordionAction.style.color = isChecked ? "#ff0032" : "";
-  topSection.style.backgroundColor = isChecked ? "#fff" : "";
-  navigation.style.backgroundColor = isChecked ? "#fff" : "";
-  fullHeader.style.boxShadow = isChecked
-    ? "0 0 16px rgba(0, 0, 0, 0.08), 0 4px 16px rgba(0, 0, 0, 0.08)"
-    : "";
+  document.querySelectorAll(".desktop-nav__list-header").forEach((header) => {
+    const accordionName = header.querySelector(".desktop-nav__header-name");
+    const accordionArrow = header.querySelector("svg");
+    const content = header.nextElementSibling;
+    content.style.maxHeight = "";
+    accordionName.style.color = "";
+    accordionArrow.style.transform = "";
+    accordionArrow.style.color = "";
+  });
 }
 
-function isBigScreen() {
-  if (window.innerWidth > 1281) {
-    accordionHeaders.forEach((header) => {
-      const checkbox = header.querySelector(".hidden-checkbox");
-      const isChecked = checkbox.checked;
+dropoutNames.forEach((name) => {
+  const dropoutButton = document.querySelector(`.${name}-dropout`);
 
-      if (isChecked) {
-        topSection.style.backgroundColor = "#fff";
-      }
-    });
-
-    dropoutNames.forEach((name) => {
-      const accordion = document.querySelector(`.${name}-dropout`);
-      accordion.dataset.name = name;
-
-      accordion.addEventListener("change", showMenu);
-
-      accordionHeaders.forEach((header) => {
-        const checkbox = header.querySelector(".hidden-checkbox");
-
-        checkbox.addEventListener("change", handleCheckboxChange);
-      });
-    });
-
-    function changeMenuSection() {
-      radioNames.forEach((radioName) => {
-        const content = document.querySelector(
-          `.base-menu-content-${radioName}`
-        );
-        if (content) {
-          content.style.display = "none";
-        }
-      });
-
-      radioNames.forEach((radioName) => {
-        const radio = document.querySelector(`.big-menu-${radioName}-radio`);
-        const content = document.querySelector(
-          `.base-menu-content-${radioName}`
-        );
-
-        if (radio && radio.checked && content) {
-          content.style.display = "block";
-        }
-      });
-    }
-
-    changeMenuSection();
-
-    radioNames.forEach((radioName) => {
-      const radio = document.querySelector(`.big-menu-${radioName}-radio`);
-      if (radio) {
-        radio.addEventListener("change", changeMenuSection);
-      }
-    });
-
-    const bigMenuLabels = document.querySelectorAll(".big-menu-label");
-
-    bigMenuLabels.forEach((label) => {
-      const radio = label.previousElementSibling;
-
-      function styleSections() {
-        bigMenuLabels.forEach((label) => {
-          label.style.backgroundColor = label.previousElementSibling.checked
-            ? "#1d2023"
-            : "#fff";
-          label.style.color = label.previousElementSibling.checked
-            ? "#fafafa"
-            : "#1d2023";
-        });
-      }
-
-      styleSections();
-
-      radio.addEventListener("change", () => {
-        styleSections();
-        if (!radio.checked) {
-          label.style.backgroundColor = "#fff";
-          label.style.color = "#1d2023";
-        }
-      });
-    });
-  } else {
-    if (threeLineCheckbox.checked) {
-      topSection.style.backgroundColor = "#fff";
-      topSection.style.boxShadow =
-        "0 0 6px rgba(0,0,0,.08),0 4px 16px rgba(0,0,0,.08)";
+  dropoutButton.addEventListener("click", () => {
+    dropoutCheckers[`.${name}-dropout`] = !dropoutCheckers[`.${name}-dropout`];
+    const menu = document.querySelector(`.${name}-menu`);
+    const isClicked = dropoutCheckers[`.${name}-dropout`];
+    const header = dropoutButton.parentElement;
+    const content = header.nextElementSibling;
+    const accordionName = header.querySelector(".desktop-nav__header-name");
+    const accordionArrow = header.querySelector("svg");
+    if (isClicked) {
+      resetButtons();
+      dropoutCheckers[`.${name}-dropout`] = true;
+      menu.classList.add("active-menu");
+      content.style.maxHeight = "3000px";
+      accordionName.style.color = "#ff0032";
+      accordionArrow.style.transform = "rotate(-180deg)";
+      accordionArrow.style.color = "#ff0032";
     } else {
-      topSection.style.backgroundColor = "#f2f3f7";
+      menu.classList.remove("active-menu");
+      content.style.maxHeight = "";
+      accordionName.style.color = "";
+      accordionArrow.style.transform = "";
+      accordionArrow.style.color = "";
     }
 
-    dropoutNames.forEach((name) => {
-      document
-        .querySelector(`.${name}-dropout`)
-        .removeEventListener("change", showMenu);
-    });
+    topSection.style.backgroundColor = isClicked ? "#fff" : "";
+    navigation.style.backgroundColor = isClicked ? "#fff" : "";
+    fullHeader.style.boxShadow = isClicked
+      ? "0 0 16px rgba(0, 0, 0, 0.08), 0 4px 16px rgba(0, 0, 0, 0.08)"
+      : "";
+  });
+});
 
-    accordionHeaders.forEach((header) => {
-      const checkbox = header.querySelector(".hidden-checkbox");
+function changeMenuSection() {
+  radioNames.forEach((radioName) => {
+    const content = document.querySelector(`.base-menu-content-${radioName}`);
+    if (content) {
+      content.style.display = "none";
+    }
+  });
 
-      checkbox.removeEventListener("change", handleCheckboxChange);
+  radioNames.forEach((radioName) => {
+    const radio = document.querySelector(`.big-menu-${radioName}-radio`);
+    const content = document.querySelector(`.base-menu-content-${radioName}`);
+
+    if (radio && radio.checked && content) {
+      content.style.display = "block";
+    }
+  });
+}
+
+changeMenuSection();
+
+radioNames.forEach((radioName) => {
+  const radio = document.querySelector(`.big-menu-${radioName}-radio`);
+  if (radio) {
+    radio.addEventListener("change", changeMenuSection);
+  }
+});
+
+const bigMenuLabels = document.querySelectorAll(".big-menu-label");
+
+bigMenuLabels.forEach((label) => {
+  const radio = label.previousElementSibling;
+
+  function styleSections() {
+    bigMenuLabels.forEach((label) => {
+      label.style.backgroundColor = label.previousElementSibling.checked
+        ? "#1d2023"
+        : "#fff";
+      label.style.color = label.previousElementSibling.checked
+        ? "#fafafa"
+        : "#1d2023";
     });
   }
-}
 
-isBigScreen();
+  styleSections();
 
-window.addEventListener("resize", isBigScreen);
+  radio.addEventListener("change", () => {
+    styleSections();
+    if (!radio.checked) {
+      label.style.backgroundColor = "#fff";
+      label.style.color = "#1d2023";
+    }
+  });
+});
